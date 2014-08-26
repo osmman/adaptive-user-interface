c33c3df6c47bda3b02373e49d48a009f19987bc3
Tomáš Turek <osmman@gmail.com>
2014-08-26 21:15:29 +0200

    locale generator for xulrunner

--- aspectfaces-xul/src/main/java/cz/cvut/fel/aui/utils/LocaleGenerator.java
+++ aspectfaces-xul/src/main/java/cz/cvut/fel/aui/utils/LocaleGenerator.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class LocaleGenerator {
 
     public static void main(String[] args) throws IOException {
-        Locale[] locales = {Locale.US, Locale.forLanguageTag("CZ")};
+        Locale[] locales = {Locale.US, new Locale("cs","CZ")};
         LocaleGenerator generator = new LocaleGenerator();
 
         for (Locale locale : locales) {
@@ -18,10 +18,11 @@ public class LocaleGenerator {
 
     public void generateLocaleFile(Locale locale) throws IOException {
         Properties properties = new Properties();
-        InputStream input = this.getClass().getClassLoader().getResourceAsStream("/bundle/locale_" + locale.getLanguage() + ".properties");
+        InputStream input = new FileInputStream(new File(String.format("bundle/src/main/resources/bundle/locale_%s.properties",locale.getLanguage())));
         properties.load(input);
 
-        OutputStreamWriter output = new OutputStreamWriter(new FileOutputStream(String.format("src/main/xulrunner/chrome/locale/%s_%s/locale.dtd", locale.getLanguage(), locale.getCountry())));
+
+        OutputStreamWriter output = new OutputStreamWriter(new FileOutputStream(String.format("aspectfaces-xul/src/main/xulrunner/chrome/locale/%s-%s/locale.dtd", locale.getLanguage(), locale.getCountry())));
         for (Map.Entry<Object, Object> entry : properties.entrySet()) {
             output.append(String.format("<!ENTITY %s \"%s\" >", entry.getKey(), entry.getValue()));
             output.append("\r\n");
